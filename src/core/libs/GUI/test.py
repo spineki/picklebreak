6fdbedf45c6bfb49e8213fbbf9dd71ce451f93ac
Victor Collodel <victor-collodel@orange.fr>
2019-11-18 11:44:27 +0100

    test gui mise en page + output

--- src/core/libs/GUI/test.py
+++ src/core/libs/GUI/test.py
@@ -6,25 +6,27 @@ class Application(tk.Frame):
         super().__init__(master)
         self.master = master
         self.master.title("Pickle Break")
-        self.create_widgets(n)
+        self.create_widgets()
 
 
     def create_widgets(self):
 
-        self.text = tk.StringVar(self.master, "Texte de base")
-        self.game_frame_hint = tk.Frame(self.master, width=300, height=300)
-        self.game_frame_input = tk.Frame(self.master, width=300, height=300, borderwidth=50)
+        self.game_frame_hint = tk.Frame(self.master, width=300, height=200, bg="red")
+        self.game_frame_input = tk.Frame(self.master, width=300, height=200, bg="yellow")
+        self.game_frame_output = tk.Frame(self.master, bg="black", height=200)
 
         self.myscrollbar= tk.Scrollbar(self.game_frame_input,orient="vertical")
-        self.input_test = tk.Text(self.game_frame_input,bg="gray")
+        self.input_test = tk.Text(self.game_frame_input,bg="white", width=50)
         self.myscrollbar.config(command=self.input_test.yview)
         self.input_test.config(yscrollcommand=self.myscrollbar.set)
         
-        self.text_print = tk.Text(self.input_test, bg="gray")
-        self.text_print.insert(0.0, 'first text', ('important'))
+        self.output_text = tk.Text(self.game_frame_output, bg="black", fg="white")
+        self.text_print = tk.Text(self.input_test, bg="white", height=2)
+        self.text_print.insert(0.0, 'Texte immuable', ('important'))
         self.text_print.config(state="disabled")
+        self.text_print.pack()
         self.input_test.window_create(index=0.0, window=self.text_print)
-        self.input_test.insert(1.0, 'Ecrire ici')
+        self.input_test.insert(2.0, "#votre vode ici")
 
         self.bp_exec = tk.Button(self.game_frame_input,text="Execute",fg='navy',command=self.exec_fct)
         self.bp_exec.pack(side= "bottom",padx=10,pady=10)
@@ -32,13 +34,19 @@ class Application(tk.Frame):
         self.myscrollbar.pack(side="right",fill="y")
 
         self.input_test.pack()
-        self.game_frame_hint.pack(side="left")
-        self.game_frame_input.pack(side="right")
+        self.output_text.pack()
+
+        self.game_frame_hint.grid(row=0, column=0, sticky="nesw")
+        self.game_frame_input.grid(row=0, column=1, sticky='nesw')
+        self.game_frame_output.grid(row=1, column=0, columnspan=2, sticky ="nesw")
+        #self.game_frame_hint.pack(side="left")
+        #self.game_frame_input.pack(side="right")
+        #self.game_frame_output.pack(side="bottom")
 
 
 
     def exec_fct(self):
-        print("toto")
+        self.output_text.insert("end", self.input_test.get("1.0","end"))
 
 
 def Pickle_graphical_grid_init():
