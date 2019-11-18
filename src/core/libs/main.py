bb50f4e111f0fcd38e7a94bebb8555ed3d1efcb0
Marras Antoine <antoine.marras@student-cs.fr>
2019-11-18 16:34:23 +0100

    class levelLoader deleted

--- src/core/libs/main.py
+++ src/core/libs/main.py
@@ -2,12 +2,12 @@ import sys
 from io import StringIO
 
 from src.core.libs.GUI.test import Application
-from src.core.libs.level_loader import LevelLoader
+
 
 class Core:
     def __init__(self):
         # self.window = Application()
-        self.level_loader = LevelLoader()
+        pass
 
     def handle_user_code(self):
         """
