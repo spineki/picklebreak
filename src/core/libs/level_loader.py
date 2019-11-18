c28f63b63336b10ddb3d356543e464877ba080b4
Remon Majoor <remon.majoor@student-cs.fr>
2019-11-18 15:53:44 +0100

    level_loader, level.json changes, .gitignore ssh_key_add and remove the file.json which was useless

--- src/core/libs/level_loader.py
+++ src/core/libs/level_loader.py
@@ -1,6 +1,26 @@
 import json
+file = "level.json"
+level = "1"
+
+def json_reader(file,level):
+    fichier = open(file,'r')
+    dic = json.load(fichier)
+    print(dic[level])
+
+def json_writer(file,clue=None,level=None,imports=[]):
+    fichier = open(file,'r')
+    dic = json.load(fichier)
+    fichier = open(file,'w')
+    if level == None: #Create a new level if level == None
+        L = list(dic.keys())
+        for i in range(len(L)):
+            L[i] = int(L[i])
+        maximum = max(L) #Search for maximum to create a new level
+        dic[str(maximum)]={"id":str(maximum),"clue":clue,"imports":imports}
+    else:
+        dic[str(level)]={"id":str(level),"clue":clue,"imports":imports}
+    json.dump(file,dic)
+        
 
 
-fichier = open("file.json",'r')
-test = json.load(fichier)
-print(test)
+json_writer(file,clue="new clue")
