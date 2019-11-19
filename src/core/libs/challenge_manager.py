c350589620701d4c1a7fac52d3997159207a2d43
Yohann B <bosq.yohann@gmail.com>
2019-11-19 02:15:44 +0100

    Adding comments. (Object not tested)

--- src/core/libs/challenge_manager.py
+++ src/core/libs/challenge_manager.py
@@ -1,3 +1,7 @@
+"""
+    Master library for challenge management.
+"""
+
 from src.core.libs.user_code_manager import run_user_script
 
 class Challenge ():
@@ -5,21 +9,36 @@ class Challenge ():
     def __init__ (self, level, win_frame):
         self.level = level
         self.win_frame = win_frame
+        self.key = ""
         self.objs = []
     
-    def reset (self, key):
+    def reset (self):
+        """
+            Reset function called on first load, refresh and pre-code execution.
+        """
+
+        self.key = "" # NEW KEY GEN
+
         ext_hints = [h[1] for h in self.level.hints]
-        self.new_hints, self.new_scripts, self.objs = self.level.gen(key, ext_hints, self.level.scripts)
+        self.new_hints, self.new_scripts, self.objs = self.level.gen(self.key, ext_hints, self.level.scripts)
 
         parsed = [(self.level.hints[i][0], self.new_hints[i]) for i in range(len(self.level.hints))]
         # UPDATE WIN_FRAME
     
-    def out (self, key):
-        self.level.close(key, self.objs)
+    def out (self):
+        """
+            Out / close function called on leaving or pre-code execution.
+        """
+
+        self.level.close(self.key, self.objs)
     
-    def execute (self, key): # , script) SCRIPT AS PARAMETER
-        self.out(key)
-        self.reset(key)
+    def execute (self): # , script) SCRIPT AS PARAMETER
+        """
+            Code execution.
+        """
+
+        self.out()
+        self.reset()
 
         script = "" # OR RETRIEVE CODE FROM WINFRAME
 
@@ -28,4 +47,4 @@ class Challenge ():
 
         run_user_script(script, self.level.imports, {"send_key": send_key})
 
-        return extracted_value == key
+        return extracted_value == self.key
