f5dec34864d0f019008f598a4e9d8d4ef89c0e7b
Remon Majoor <remon.majoor@student-cs.fr>
2019-11-20 16:37:00 +0100

    Finished to merge and solve the problems with level_manager.py and changed test_level_manager accordingly

--- src/Tests/test_level_manager.py
+++ src/Tests/test_level_manager.py
@@ -3,11 +3,14 @@ import os, sys
 parentPath = os.path.abspath("..")
 if parentPath not in sys.path:
     sys.path.insert(0, parentPath)
-from core.libs.level_manager import load_level,write_level,Level
+from core.libs.level_manager import Level
 
 def test_load_level():
-    level = Level("pickle_level",file = "json_test_file.json")
-    #level = Level(json_data)
+    """ 
+    Test of the data of a level from the json file
+    """
+
+    level = Level("pickle_level",levels_file = "json_test_file.json") 
     assert level.name == "26"
     assert level.next == "27"
     assert level.scripts == ["print(\"This script cannot be modified\")",""]
@@ -15,11 +18,14 @@ def test_load_level():
     assert level.hints == [('text', "WOW!!! The key is {}")]
 
 def test_write_level():
-    level = Level("pickle_level",file = "json_test_file.json")
+    """
+    Test to write in the json file
+    """
+    
+    level = Level("pickle_level",levels_file = "json_test_file.json")
+    level2 = Level("pickle_level",levels_file = "json_test_file.json")
     level.name = "34"
-    Level.write(level, file = "json_test_file.json")
+    Level.write(level, levels_file = "json_test_file.json")
     level.name = "45"
-    level.write(file = "json_test_file.json")
-    json_data = load_level("34",file = "json_test_file.json")
-    level2=Level(json_data)
+    level.write(level,levels_file = "json_test_file.json")
     assert level2 != level
---
+++ src/res/levels/backend_test.py
@@ -0,0 +1,5 @@
+def gen(*args):
+    return tuple(args[1:3])
+
+def close (*args):
+    pass
