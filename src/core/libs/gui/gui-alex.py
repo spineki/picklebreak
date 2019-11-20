93000319e7d6b13b0c78315d1e064b65eb94c4e5
Alexandre Le Bian <alexandre.le-bian@student-cs.fr>
2019-11-20 10:11:40 +0100

    my beautiful gui sans fautes

--- src/core/libs/gui/gui-alex.py
+++ src/core/libs/gui/gui-alex.py
@@ -1,112 +1,107 @@
-import tkinter as tk
-from PIL import Image, ImageTk
+import tkinter as tk 
 
 class Application(tk.Frame):
-    def __init__(self, master=None):
+    def __init__(self, text, master=None):
 
         super().__init__(master)
         self.master = master
         self.master.title("Pickle Break")
-        self.create_widgets()
+        self.init_frames()
+        self.init_note_bloc()        
+        self.init_level(text)
 
 
-    def create_widgets(self):
+    def init_frames(self):
 
+        #Generation of the 3 principales frames
         self.game_frame_hint = tk.Frame(self.master, width=300, height=200, bg="red")
         self.game_frame_input = tk.Frame(self.master, width=300, height=200, bg="yellow")
         self.game_frame_output = tk.Frame(self.master, bg="black", height=200)
 
+    def init_note_bloc(self):
+        #Generation of the text zone and its scrollbar
         self.myscrollbar= tk.Scrollbar(self.game_frame_input,orient="vertical")
         self.input_test = tk.Text(self.game_frame_input,bg="white", width=50)
         self.myscrollbar.config(command=self.input_test.yview)
         self.input_test.config(yscrollcommand=self.myscrollbar.set)
-        
-        self.output_text = tk.Text(self.game_frame_output, bg="black", fg="white")
-        self.text_print = tk.Text(self.input_test, bg="white", height=2)
-        self.text_print.insert(0.0, 'Texte immuable', ('important'))
-        self.text_print.config(state="disabled")
-        self.text_print.pack()
-        self.input_test.window_create(index=0.0, window=self.text_print)
-        self.input_test.insert(2.0, "#votre vode ici")
-
-        self.bp_exec = tk.Button(self.game_frame_input,text="Execute",fg='navy',command=self.exec_fct)
-        self.bp_exec.pack(side= "bottom",padx=10,pady=10)
-
         self.myscrollbar.pack(side="right",fill="y")
 
-        self.input_test.pack()
-        self.output_text.pack()
-
-        img = ImageTk.PhotoImage(Image.open("./src/res/pickle.png"))
-        self.image=tk.Label(self.game_frame_hint, image=img)
-        self.image.image = img
-        self.image.pack()
-
+        #Generate the Frames in a grid in ordre to have a good disposition
         self.game_frame_hint.grid(row=0, column=0, sticky="nesw")
         self.game_frame_input.grid(row=0, column=1, sticky='nesw')
         self.game_frame_output.grid(row=1, column=0, columnspan=2, sticky ="nesw")
-        #self.game_frame_hint.pack(side="left")
-        #self.game_frame_input.pack(side="right")
-        #self.game_frame_output.pack(side="bottom")
+        self.input_test.pack()
 
-    def get_input(self):
+    def init_level(self, text):
+        #Use of the display_text function which will add in the Text widget, Text widgets with the right organisation
+        self.display_text(text)
+
+        #Those lines make the principale Text zone unchangeable (if it was not, the user could delete the Text zones generated before inside this Text zone)
+        self.input_test.insert(0.0, "")
+        self.input_test.config(state="disabled")
+
+        #Creation of the Buttons and linked to their functions
+        self.bp_exec = tk.Button(self.game_frame_input,text="Execute",fg='navy')
+        self.bp_reset = tk.Button(self.game_frame_input,text="Reset",fg='navy',command=self.reset_fct)
+        self.bp_exec.pack(side= "bottom",padx=10,pady=10)
+        self.bp_reset.pack(side= "bottom",padx=10,pady=10)
+       
+        
+    def display_text(self, text):
         """
-        Récupère le texte entré par le joueur
-        ARGS : 
-            None
-        RETURN:
-            texte -> string : texte entrée par l'utilisateur dans le champ bloc-note
-        """
-        return self.input_test.get("1.0","end")
-    
-    def display_output(self, texte):
-        """
-        Affiche un texte dans la zone sortie
+        This function display the texts zones where the informations are printed and where the user can write
         ARGS:
-            texte -> string : texte à afficher
-        RETURN:
-            None
+        text: str: Contains the informations which will be printed (nb: A string empty (ie "") represent a zone where the user can write)
+        RETURNS:
+        None
         """
-        self.output_text.config(state="normal")
-        self.output_text.insert("end", texte)
-        self.output_text.config(state="disabled")
+        self.text_zones = [" "]*len(text)
+        for i in range(len(text)):
+            
+            if text[i] != "":
+                self.text_zones[i] = tk.Text(self.input_test, bg="tomato", height=2)
+                self.text_zones[i].insert("end", text[i])
+                self.text_zones[i].config(state="disabled")
+                
+            else:
+                self.text_zones[i] = tk.Text(self.input_test, bg="white", height=2)
+                self.text_zones[i].insert("end", "#You can write here")
+
+            self.text_zones[i].pack()
+            self.input_test.window_create(index="end", window=self.text_zones[i])
+        return(self.text_zones)
+
+    def get_code(self):
+        self.output = []
+        for i in range(len(self.text_zones)):
+            self.output.append(self.text_zones[i].get(0.0, "end"))
+        self.output = "\n".join(self.output)
+        return self.output
     
-    def display_hint(self, list_to_display):
-        self.image = None
-        for thing in list_to_display:
-            if thing[0]=="txt":
-                pass #à faire
-            elif thing[0]=="img" and isinstance(thing[1], str):
-                img = ImageTk.PhotoImage(Image.open("./src/res/pickle.png"))
-                self.image=tk.Label(self.game_frame_hint, image=img)
-                self.image.image = img
-                self.image.pack()
-            elif thing[0]=="img" and isinstance(thing[1], Image.Image):
-                img = ImageTk.PhotoImage(thing[1])
-                self.image=tk.Label(self.game_frame_hint, image=img)
-                self.image.image = img
-                self.image.pack()
+    def set_exec_fct(self, function):
+        self.bp_exec.config(command= function)
+
+    def set_reset_fct(self, function):
+        self.bp_reset.config(command = function)
+
+    def reset_fct(self):
+        for widget in self.game_frame_output.winfo_children():
+            widget.destroy()
+        for widget in self.game_frame_input.winfo_children():
+            widget.destroy()
+        for widget in self.game_frame_hint.winfo_children():
+            widget.destroy()
+        self.init_frames()
+        self.init_note_bloc()        
+        self.init_level(text)
 
 
-    def exec_fct(self):
-        """
-        Fonction qui s'éxécute quand le joueur clique sur le bouton execute
-        """
-        self.output_text.config(state="normal")#débloque la zone de sortie pour pouvoir afficher
-
-        self.output_text.insert("end", self.input_test.get("1.0","end"))#exemple, à modifier
-
-        self.output_text.config(state="disabled")#rebloque la zonne de sortie pour ne pas que le joueur puisse écrire dans la sortie
-
-
-
-def Pickle_graphical_grid_init():
+def Pickle_graphical_grid_init(text):
     root = tk.Tk()
     root.geometry("800x800")
-    app = Application(master=root)
+    app = Application(text ,master=root)
     #app.change_values([[0,1],[1,0]], 2)
     app.mainloop()
-
-if __name__ == '__main__':
-
-    Pickle_graphical_grid_init()
+    
+text = ["Coucou","","Tu peux écrire?", "", "", "", "", "", ""]
+Pickle_graphical_grid_init(text)
