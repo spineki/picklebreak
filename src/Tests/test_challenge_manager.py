088df6ebe3db474f772164629fd1bcbe1325cd70
Remon Majoor <remon.majoor@student-cs.fr>
2019-11-21 16:40:30 +0100

    test_challenge_manager is added and works and changed level_manager

---
+++ src/Tests/test_challenge_manager.py
@@ -0,0 +1,6 @@
+from pytest import *
+import os, sys
+parentPath = os.path.abspath("..")
+if parentPath not in sys.path:
+    sys.path.insert(0, parentPath)
+from core.libs.challenge_manager import Challenge
